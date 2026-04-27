38c396721c3966abc7fb6fead37fab14df0a2bc1
mbsquared <mike.bushell@gmail.com>
2026-04-27 09:03:08 +0100

    Removal of float check and D3D12 line render width

--- drivers/d3d12/rendering_device_driver_d3d12.cpp
+++ drivers/d3d12/rendering_device_driver_d3d12.cpp
@@ -5176,8 +5176,8 @@ RDD::PipelineID RenderingDeviceDriverD3D12::render_pipeline_create(
 	}
 	(&pipeline_desc.RasterizerState)->ForcedSampleCount = 0;
 	(&pipeline_desc.RasterizerState)->ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
-	(&pipeline_desc.RasterizerState)->MultisampleEnable = TEXTURE_SAMPLES_COUNT[p_multisample_state.sample_count] != 1;
-	(&pipeline_desc.RasterizerState)->AntialiasedLineEnable = true;
+	(&pipeline_desc.RasterizerState)->MultisampleEnable = false;
+	(&pipeline_desc.RasterizerState)->AntialiasedLineEnable = false;
 
 	// In D3D12, there's no line width.
 	ERR_FAIL_COND_V(!Math::is_equal_approx(p_rasterization_state.line_width, 1.0f), PipelineID());
--- scene/property_utils.cpp
+++ scene/property_utils.cpp
@@ -43,7 +43,8 @@
 bool PropertyUtils::is_property_value_different(const Object *p_object, const Variant &p_a, const Variant &p_b) {
 	if (p_a.get_type() == Variant::FLOAT && p_b.get_type() == Variant::FLOAT) {
 		// This must be done because, as some scenes save as text, there might be a tiny difference in floats due to numerical error.
-		return !Math::is_equal_approx((float)p_a, (float)p_b);
+		//return !Math::is_equal_approx((float)p_a, (float)p_b);
+		return true;
 	} else if (p_a.get_type() == Variant::NODE_PATH && p_b.get_type() == Variant::OBJECT) {
 		// With properties of type Node, left side is NodePath, while right side is Node.
 		const Node *base_node = Object::cast_to<Node>(p_object);
